74529db5af38c1513261bb77444cad715d1c8251
Soarez <igor.soarez@rupeal.com>
2012-04-23 19:22:41 +0100

    First commit

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-
-/**
- * Module dependencies.
- */
+require('console-trace')
+console.traceAlways = true;
 
 var express = require('express')
-  , redis = require("redis").createClient()
-  //, RedisStore = require('connect-redis')(express)
   , routes = require('./routes')
-
-
-var redisWrapper = {
-  get: function(key, callback) {
-    redis.get(key, function(error, value) {
-      if (error) console.log('Redis error: ' + error);
-      else callback(JSON.parse(value));
-    });
-  },
-
-  set: function(key, value) {
-    redis.set(key, JSON.stringify(value));
-  }
-};
+  , socketio = require('socket.io')
+  , lessMiddleware = require('less-middleware')
+  , ejsLayoutSupport = require('./ejsLayoutSupport')
 
 var app = module.exports = express.createServer();
-
-// Configuration
+var io = socketio.listen(app);
 
 app.configure(function(){
-  
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(lessMiddleware({
+         src: __dirname + '/public',
+         force: true,
+         once: false,
+         debug: true,
+         compress: false,
+     }));
   app.use(express.static(__dirname + '/public'));
   app.use(express.cookieParser());
-  //app.use(express.session({ secret: "read_this_froma_file_listed_in_gitignore", store: new RedisStore }));
-  //app.use(express.csrf());
+  app.use(ejsLayoutSupport);
   app.use(app.router);
   
   app.use(function(err, req, res, next){
@@ -49,23 +38,37 @@ app.configure(function(){
   app.use(function(req, res){
      res.render('404.html.ejs');
   });
-  
-  app.session = redisWrapper;
-  //app.helpers({});
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.session = require('./app_session');
 });
 
-app.configure('production', function(){
+app.configure('staging production', function(){
   app.use(express.errorHandler());
+  app.session = require('./redis_app_session');
+  
+  app.use(lessMiddleware({
+         src: __dirname + '/public',
+         force: false,
+         once: true,
+         debug: false,
+         compress: true,
+     }));
 });
-
-// Routes
 
 app.get('/:hash?', routes.index);
 app.post('/', routes.createMeeting);
-  
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.post('/join/:hash'     , meeting.handlers.joinMeeting);
+app.post('/leave/:hash'     , meeting.handlers.leaveMeeting);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('attendee:hello', function(data){
+      // 
+    });
+});
+
+var port = app.settings.env == 'development' ? 3333 : 80;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,44 @@ module.exports.index = exports.index = function(req, res) {
   req.app.session.get(req.params.hash, function(meeting){
     if (!meeting)
       res.render('index', { title: 'Meetoring' })
-    else
-      res.render('meeting', { meeting: meeting, title: 'Meeting' });
+    else {
+      res.render('meeting', { meeting: meeting, title: 'Meeting' })
+    }
   });
 };  
+
+
+var crc32 = require('../crc32');
+
+/*
+ * POST home page
+ */
+module.exports.createMeeting = exports.createMeeting = function(req, res, next){
+
+  var name = req.body.meetingName;
+  
+  if (!name) 
+    res.render('index', { title: 'Meetoring' })
+
+  var hash = crc32(name);
+
+  req.app.session.get(hash, function(meeting) {
+  
+    if (meeting) {
+      console.log('meeting already exists');
+          // TODO: flash some warning back to the same page    
+      return;
+    }
+  
+    meeting = {
+      name: name,
+      attendees: [],
+      totalCost: 0
+    };
+
+    req.app.session.set(hash, meeting); 
+  
+    console.log('redirecting to: ' + hash)
+    res.redirect('/'+hash);
+  });
+}; 
